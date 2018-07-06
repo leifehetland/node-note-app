d7e5eb7a2b91372f11f6850441f1ac8d52a0fe19
Leif Hetland <lehetland1@gmail.com>
2018-07-05 22:14:37 -0400

    using 3rd party libraries

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ console.log('Starting app.js.');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
+
+let banana = 'Hello World';
+
+console.log(_.isString(true));
+console.log(_.isString('true'));
+console.log(_.isString(banana));
+console.log(_.uniq([1, true, true, 1, 1, 1, 1, 5, 5, 5, banana, banana, banana]));
+
 
 let res = notes.addNote();
 console.log(res);

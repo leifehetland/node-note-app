8df3c2b77d04021f89b51c1db111a7d603ce4684
Leif Hetland <lehetland1@gmail.com>
2018-07-20 13:46:30 -0400

    adding and saving notes

--- notes.js
+++ notes.js
@@ -1,7 +1,30 @@
 console.log('Starting notes.js.');
 
+const fs = require('fs');
+
 let addNote = (title, body) => {
-	console.log("Adding note: ", title, body);
+	// console.log("Adding note: ", title, body);
+	let notes = [];
+	let note = {
+		title: title,
+		body: body
+	};
+
+	try {
+		let notesString = fs.readFileSync('notes-data.json');
+		notes = JSON.parse(notesString);	
+	} catch (error) {
+		
+	}
+
+	let duplicateNotes = notes.filter((note) => {
+		return note.title === title;
+	});
+
+	if (duplicateNotes.length === 0) {
+		notes.push(note);
+		fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+	}
 };
 
 let getAll = () => {
